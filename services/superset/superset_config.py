75465e064e8f2e0803529512b55cca9a3c7149a0
yakamo <yakamo@yakamos-MacBook-Pro.local>
2020-01-12 16:50:01 +0100

    fixin network issue for airflow logs
    Airflow tries to access the docker id instead of host. It works on my 
    local machine but not on remote host. Who knows why. but creating a 
    network should fix that

--- services/superset/superset_config.py
+++ services/superset/superset_config.py
@@ -1,5 +1,7 @@
 import os
 
+from werkzeug.contrib.cache import RedisCache
+
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY', '')
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
@@ -8,7 +10,21 @@ CACHE_CONFIG = {
     'CACHE_REDIS_HOST': 'redis',
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
-    'CACHE_REDIS_URL': os.getenv('REDIS_URL')}
+    'CACHE_REDIS_URL': 'redis://:TCPYkerxvKQu@redis:6379/1'}
 SQLALCHEMY_DATABASE_URI = os.getenv('SUP_META_DB_URI')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'thisISaSECRET_1234'
+
+
+class CeleryConfig(object):
+    BROKER_URL = 'redis://redis:6379/0'
+    CELERY_IMPORTS = ('superset.sql_lab', )
+    CELERY_RESULT_BACKEND = 'redis://:TCPYkerxvKQu@redis:6379/0'
+    CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
+
+CELERY_CONFIG = CeleryConfig
+RESULTS_BACKEND = RedisCache(
+    host='redis',
+    port=6379,
+    key_prefix='superset_results'
+)
